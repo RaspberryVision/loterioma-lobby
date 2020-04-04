641127ec72068ebe353191a0e64d6f4efeaccf61
RafalMalik <rafalmalik.info@gmail.com>
2020-04-04 06:19:02 +0200

    Created post request method. Fixed engine url.

--- assets/js/dice/dice.js
+++ assets/js/dice/dice.js
@@ -1,6 +1,6 @@
 import http from '../http';
 
-const ENGINE_URL = `http://localhost:9902/index.php/play/dice`;
+const ENGINE_URL = `http://localhost:9902/index.php/dice/play`;
 
 export default class DiceClient {
 
@@ -13,11 +13,31 @@ export default class DiceClient {
     }
 
     makeRequest() {
-
-        alert('lece z requestem');
-        let response = http.requestGet(`${ENGINE_URL}/${this.config.gameId}`, []);
+        let response = http.requestPost(`${ENGINE_URL}/${this.config.gameId}`, this.getRequestParams());
 
         console.log(response);
+    }
+
+    getRequestParams() {
+        return JSON.stringify({
+            "gameId": this.config.gameId,
+            "client": this.config.client,
+            "userId": this.config.userId,
+            "mode": this.config.mode,
+            "parameters": this.getRoundParameters()
+        });
+    }
+
+    getRoundParameters() {
+        return {
+            "bets":
+                [
+                    {
+                        "number": 1,
+                        "stake": 10,
+                    }
+                ]
+        };
     }
 }
 
