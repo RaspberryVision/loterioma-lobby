e765e1229eefbba47e597755b73f40e12d0725c4
RafalMalik <rafalmalik.info@gmail.com>
2021-01-05 18:05:43 +0100

    Working with game handler.

--- src/Handler/Game/GameCreatedHandler.php
+++ src/Handler/Game/GameCreatedHandler.php
@@ -5,6 +5,7 @@ namespace App\Handler\Game;
 
 
 use App\Entity\Game;
+use App\Entity\GeneratorConfig;
 use App\Message\Game\GameCreated;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
@@ -33,6 +34,14 @@ class GameCreatedHandler implements MessageHandlerInterface
         $game->setName($object->name)
             ->setDescription($object->description)
             ->setType($object->type);
+
+        $generatorConfig = new GeneratorConfig();
+        $generatorConfig->setMin($object->generatorConfig->min)
+            ->setMax($object->generatorConfig->max)
+            ->setFormat($object->generatorConfig->format)
+            ->setSeed($object->generatorConfig->seed);
+
+        $game->setGeneratorConfig($generatorConfig);
 
         $this->entityManager->persist($game);
         $this->entityManager->flush();
