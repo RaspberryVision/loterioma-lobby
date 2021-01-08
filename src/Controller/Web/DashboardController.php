1b3d2fcbc59f3491249541154b9ae1306daa1194
RafalMalik <rafalmalik.info@gmail.com>
2021-01-08 17:29:16 +0100

    Updated menu positions.

--- src/Controller/Web/DashboardController.php
+++ src/Controller/Web/DashboardController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractController
 {
     /**
-     * @Route("/", name="web_dashboard")
+     * @Route("/", name="web_dashboard_index")
      */
     public function index()
     {
