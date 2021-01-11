ff975a244d0e07f2f9fdd2af222a5755a89cbf6b
RafalMalik <rafalmalik.info@gmail.com>
2021-01-11 21:32:27 +0100

    Added GameSyncedHandler.

---
+++ src/Handler/Game/GameSyncedHandler.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Handler\Game;
+
+use App\Entity\Game;
+use App\Message\Game\GameSynced;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+
+class GameSyncedHandler implements MessageHandlerInterface
+{
+    /**
+     * @var EntityManagerInterface $entityManager
+     */
+    private $entityManager;
+
+    /**
+     * UserRegistrationHandler constructor.
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function __invoke(GameSynced $message)
+    {
+        $object = json_decode($message->getContent());
+
+        $game = $this->entityManager->getRepository(Game::class)->find($object->id);
+
+        if (!$game) {
+            $game = new Game();
+        }
+        $game->setName($object->name)
+            ->setDescription($object->description)
+            ->setType($object->type);
+
+        $this->entityManager->persist($game);
+        $this->entityManager->flush();
+    }
+}
