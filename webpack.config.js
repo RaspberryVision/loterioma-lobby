77f7c63b907a5e2ec01c38212fc27b939f318b99
RafalMalik <rafalmalik.info@gmail.com>
2021-01-16 15:52:54 +0100

    Added basic slots view.

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ Encore
      */
     .addEntry('dice', './assets/js/dice/index.js')
 
+
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
