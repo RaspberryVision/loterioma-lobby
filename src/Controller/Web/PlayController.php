40025a61623310f128f93b9ef583c7e04c9b1b10
RafalMalik <rafalmalik.info@gmail.com>
2021-02-09 23:48:32 +0100

    Added backend action to save round in PlayController.php

--- src/Controller/Web/PlayController.php
+++ src/Controller/Web/PlayController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller\Web;
 
+use App\Entity\DiceRound;
 use App\Entity\Game;
 use App\NetworkHelper\DataStore\DataStoreHelper;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,6 +17,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class PlayController extends AbstractController
 {
     /**
+     * @Route("/backend", name="web_play_backend")
+     */
+    public function backend(Request $request, EntityManagerInterface $entityManager)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $round = new DiceRound();
+        $round->setBets([])
+            ->setMatched($data['matched'])
+            ->setResults($data['result']);
+
+        $entityManager->persist($round);
+        $entityManager->flush();
+
+        return $this->json([
+            'data' => $round->getId()
+        ]);
+    }
+
+    /**
      * @Route("/{id}", name="web_play_index")
      */
     public function index(Game $game)
@@ -21,13 +44,5 @@ class PlayController extends AbstractController
         return $this->render('play/index.html.twig', [
             'game' => $game
         ]);
-    }
-
-    /**
-     * @Route("/{id}/backend", name="web_play_backend")
-     */
-    public function backend(Request $request, Game $game)
-    {
-
     }
 }
