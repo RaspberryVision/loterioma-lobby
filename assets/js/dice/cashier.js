087df1f6dd4da222490cf96806d785f771ce5b8a
RafalMalik <rafalmalik.info@gmail.com>
2021-02-17 15:41:40 +0100

    Updated cashier script.

--- assets/js/dice/cashier.js
+++ assets/js/dice/cashier.js
@@ -12,7 +12,11 @@ export default class Cashier {
 
     payIn() {
         this.makeRequest('pay-in').then((data) => {
-            console.log(data);
+            if (data.status !== 0) {
+                alert('Error occurred');
+                console.log(data);
+                return;
+            }
             window.sessionId = data.sessionId;
             this.config.sessionId = data.sessionId;
             this.config.amount = data.amount;
